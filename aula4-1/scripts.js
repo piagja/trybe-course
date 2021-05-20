4aecc03b56ff61499b32b119e0b9325c1feeb288
GustavoPia <piagja@gmail.com>
2021-05-20 18:35:23 -0300

    Done exercise 10 from Javascript Fundamentals 4.1

--- aula4-1/scripts.js
+++ aula4-1/scripts.js
@@ -171,12 +171,29 @@ function haveOdd(a, b, c) {
 console.log('\nExercício 9')
 console.log(haveOdd(1, 3, 5))
 
-// Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// 10. Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
 // Atente que, sobre o custo do produto, incide um imposto de 20%.
 // Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
 // O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
 // valorCustoTotal = valorCusto + impostoSobreOCusto
 // lucro = valorVenda - valorCustoTotal (lucro de um produto)
+function productProfit(productCost, productValue) {
+  let taxOverProductCost = productCost * 0.2
+  let profit = productValue - taxOverProductCost - productCost
+
+  if (productCost < 0 || productValue < 0) {
+    console.log('Invalid Operation. Less than $0 product cost or value')
+  } else if (profit < 0) {
+    console.log(`Negative profit: ${profit}! Be careful`)
+  } else {
+    console.log(`You have a profit of $${profit}`)
+  }
+
+  return `Your product value: $${productValue}. Tax over a $${productCost} product: $${taxOverProductCost}.`
+}
+console.log('\nExercício 10')
+console.log(productProfit(1000, 700))
+
 // Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
 // A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
 // INSS (Instituto Nacional do Seguro Social)
