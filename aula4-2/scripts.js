049364cd5d0b2b25843f18b60730bc706b8dd530
GustavoPia <piagja@gmail.com>
2021-05-21 18:38:56 -0300

    Finished Exercises 5 - Javascript Arrays, For and For/Of

--- aula4-2/scripts.js
+++ aula4-2/scripts.js
@@ -21,21 +21,21 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
-console.log('Exercício 1')
+console.log('\nExercício 1')
 for (let index = 0; index < numbers.length; index += 1) {
   console.log(numbers[index])
 }
 
 // 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 let totalSum = 0;
-console.log('Exercício 2')
+console.log('\nExercício 2')
 for (let index = 0; index < numbers.length; index += 1) {
   totalSum += numbers[index]
 }
 console.log(totalSum);
 
 // 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-console.log('Exercicio 3')
+console.log('\nExercicio 3')
 totalSum = 0
 for (let index = 0; index < numbers.length; index += 1) {
   totalSum += numbers[index]
@@ -45,9 +45,44 @@ let mediaSum = totalSum / numbers.length
 console.log(mediaSum)
 
 // 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
-console.log('Exercício 4')
+console.log('\nExercício 4')
 if (mediaSum > 20) {
   console.log('Valor do Exercicio 3 é maior que 20')
 } else {
   console.log('Valor do Exercicio 3 é menor ou igual a 20')
+}
+
+// 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+console.log('\nExercício 5')
+let maxNumber = numbers[0]
+for (let index = 1; index < numbers.length; index += 1) {
+  if(numbers[index] > maxNumber) {
+    // adiciona o numero encontrado ao maxNumber daí
+    maxNumber = numbers[index]
+  }
+
+  // Outra forma de realizar o exercício 5, é usar o objeto Math, com o método max(...args).
+  // Este método retorna o maior valor dentre uma série de valores.
+  // Apenas comparei o numbers[index] com o maxNumber (indice 0 = valor 5) e trouxe o 100 também
+  // maxNumber = Math.max(numbers[index], maxNumber)
+}
+
+console.log(maxNumber)
+
+// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+console.log('\nExercício 5')
+let odds = 0
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    // odds segura o tanto de ímpares que existe na lista
+    odds += 1
+  }
+}
+
+if (odds === 0) {
+  // se a lista de impares for 0, ou seja, não conter um numero impar sequer, retorna a msg
+  console.log('nenhum número impar encontrado')
+} else {
+  // do contrário, retorna quantos impares tem
+  console.log(odds)
 }
