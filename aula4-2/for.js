6a4b7cfc7182d9fb96dfddd8d296a84ee91211cb
GustavoPia <piagja@gmail.com>
2021-05-21 17:22:07 -0300

    Done exercise 1 from Array(for) at javascript 4.2

---
+++ aula4-2/for.js
@@ -0,0 +1,12 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index = 0; index < cars.length; index += 1) {
+  console.log(cars[index]) // com o index, ele passa por cada valor
+}
+console.log('')
+// exercicio 1
+// Utilize o for para imprimir os elementos da lista groceryList com o console.log() :
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for (let index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index])
+}
