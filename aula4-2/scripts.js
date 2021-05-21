0b88ef106b43907da3d303ded715c9c9897cff15
GustavoPia <piagja@gmail.com>
2021-05-21 17:12:21 -0300

    Done exercise 2 from Array(lists) at javascript 4.2

--- aula4-2/scripts.js
+++ aula4-2/scripts.js
@@ -5,3 +5,10 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices);
+
+// Exercício 2
+// Procure o índice do valor "Portfólio" do array menu :
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
