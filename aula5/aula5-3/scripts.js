01802b6fc8d0f5cd79ae1116b30bbf1dce40d5fa
GustavoPia <piagja@gmail.com>
2021-05-28 18:37:41 -0300

    Realizou exercício 1 - JavaScript DOM - Event Listeners - 5-3

--- aula5/aula5-3/scripts.js
+++ aula5/aula5-3/scripts.js
@@ -14,3 +14,20 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const daysContainer = document.querySelector('#days');
+
+for (let index = 0; index < dezDaysList.length - 1; index += 1) {
+  const li = document.createElement('li');
+  const numeroDia = dezDaysList[index];
+
+  li.setAttribute('class', 'day');
+  li.innerHTML = numeroDia;
+
+  days.appendChild(li);
+
+  if (numeroDia === 25) li.setAttribute('class', 'day holiday friday');
+  if (numeroDia === 24 || numeroDia === 31) li.setAttribute('class', 'day holiday');
+  if (numeroDia === 4 || numeroDia === 11 || numeroDia === 18) li.setAttribute('class', 'day friday');
+}
+
