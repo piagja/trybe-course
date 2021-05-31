4c4d26f369cab721f04055ad2d78e66bf6c589c3
GustavoPia <piagja@gmail.com>
2021-05-31 16:24:00 -0300

    Realizou exercício 5 - JavaScript DOM - Event Listeners - 5-3

--- aula5/aula5-3/scripts.js
+++ aula5/aula5-3/scripts.js
@@ -1,10 +1,10 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-  const weekDaysList = document.querySelector('.week-days');
+  const weekDays = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
+  const weekDaysList = document.querySelector(".week-days");
 
   for (let index = 0; index < weekDays.length; index += 1) {
     const days = weekDays[index];
-    const dayListItem = document.createElement('li');
+    const dayListItem = document.createElement("li");
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
@@ -25,37 +25,49 @@ function onLoad () {
 
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-const daysContainer = document.querySelector('#days');
+const daysContainer = document.querySelector("#days");
 
 for (let index = 0; index < dezDaysList.length; index += 1) {
-  const li = document.createElement('li');
+  const li = document.createElement("li");
   const numeroDia = dezDaysList[index];
 
-  li.setAttribute('class', 'day');
+  li.setAttribute("class", "day");
   li.innerHTML = numeroDia;
 
   daysContainer.appendChild(li);
 
-  if (numeroDia === 25) li.setAttribute('class', 'day holiday friday');
-  if (numeroDia === 24 || numeroDia === 31) li.setAttribute('class', 'day holiday');
-  if (numeroDia === 4 || numeroDia === 11 || numeroDia === 18) li.setAttribute('class', 'day friday');
+  if (numeroDia === 25) li.setAttribute("class", "day holiday friday");
+  if (numeroDia === 24 || numeroDia === 31) li.setAttribute("class", "day holiday");
+  if (numeroDia === 4 || numeroDia === 11 || numeroDia === 18) li.setAttribute("class", "day friday");
 }
 
-function holidays(string) {
-  const btnContainer = document.querySelector('.buttons-container');
-  const btn = document.createElement('button');
-  btn.setAttribute('id', 'btn-holiday');
+/**
+ * grandes funções, repetidas, que precisam ser melhoradas
+ * uma pra criar, receber escuta do click e setar highlight
+ * nos dias determinados, sendo o primeiro feriados e o segundo
+ * esta dando highlight nas sextas-feiras, entretanto, o dia 25
+ * que é o dia que tem todas as classes (day, holiday e friday)
+ * ele desativa junto com outro botão, então nunca fica selecionado
+ * se ambos os botões forem clicados.
+ * TODO: tenho que melhorar aqui.
+ * este código foi feito através do material da Trybe, MDN e W3S
+ */
+
+function holidays (string) {
+  const btnContainer = document.querySelector(".buttons-container");
+  const btn = document.createElement("button");
+  btn.setAttribute("id", "btn-holiday");
   btn.innerText = string;
   btnContainer.appendChild(btn);
 }
 
 function clickToHolidays () {
-  const btn = document.querySelector('#btn-holiday');
-  btn.addEventListener('click', () => setHighlightHolidays());
+  const btn = document.querySelector("#btn-holiday");
+  btn.addEventListener("click", () => setHighlightHolidays());
 }
 
 function setHighlightHolidays () {
-  const li = document.getElementsByClassName('day');
+  const li = document.getElementsByClassName("day");
   for (let index = 0; index < li.length; index += 1) {
     const day = li[index];
     const holidays = day.classList.contains("holiday");
@@ -66,7 +78,7 @@ function setHighlightHolidays () {
   }
 }
 
-function fridays(string) {
+function fridays (string) {
   const btnContainer = document.querySelector(".buttons-container");
   const btn = document.createElement("button");
   btn.setAttribute("id", "btn-friday");
@@ -75,18 +87,20 @@ function fridays(string) {
 }
 
 function clickToFridays () {
-  const btn = document.querySelector('#btn-friday');
+  const btn = document.querySelector("#btn-friday");
   btn.addEventListener("click", () => setHighlightFridays());
 }
 
 function setHighlightFridays () {
-  const li = document.getElementsByClassName('day');
+  const li = document.getElementsByClassName("day");
   for (let index = 0; index < li.length; index += 1) {
     const day = li[index];
-    const holidays = day.classList.contains("friday");
-
-    if (holidays === true) {
+    const fridays = day.classList.contains("friday");
+    if (fridays === true) {
       day.classList.toggle("highlight");
+      day.classList.toggle("active-friday");
     }
   }
 }
+
+/* fim das funções em relação a anotar o feriado e sextas-feiras */
