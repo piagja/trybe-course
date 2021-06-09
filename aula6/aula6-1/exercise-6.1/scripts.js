d6560ff571fcbb1e90c1f7d1c663c3b4de8c3810
Gustavo Pia <piagja@gmail.com>
2021-06-09 19:45:07 -0300

    Atendeu ao primeiro requisito

---
+++ aula6/aula6-1/exercise-6.1/scripts.js
@@ -0,0 +1,9 @@
+const states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
+const select = document.querySelector("#input-estado");
+
+states.forEach((item) => {
+  const options = document.createElement("option");
+  options.innerHTML += `${item}`;
+  select.appendChild(options);
+});
+
