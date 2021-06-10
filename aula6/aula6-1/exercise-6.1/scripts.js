8c534d7a81b01c90746c535967b6f9e26615ebc7
Gustavo Pia <piagja@gmail.com>
2021-06-10 16:50:39 -0300

    Implementou a exibição de alguns valores do formulario

--- aula6/aula6-1/exercise-6.1/scripts.js
+++ aula6/aula6-1/exercise-6.1/scripts.js
@@ -11,7 +11,44 @@ function populateSelect() {
 
 populateSelect();
 
-function submitData (event) {
-  event.preventDefault();
+function clearData () {
+  const form = document.querySelector("#input-form");
+  form.reset();
+}
+
+function mountData () {
+  const inputName = document.querySelector("#input-name").value;
+  const inputEmail = document.querySelector("#input-email").value;
+  const inputCPF = document.querySelector("#input-cpf").value;
+  const inputEndereco = document.querySelector("#input-endereco").value;
+  const inputCidade = document.querySelector("#input-cidade").value;
+  const inputEstado = document.querySelector("#input-estado").value;
+  const inputImovelCasa = document.querySelector("#input-imovel-casa").checked;
+  const inputImovelAPT = document.querySelector("#input-imovel-apt").checked;
+  const dataArea = document.querySelector("#all-data");
+
+  const div = document.createElement("div");
+  div.classList.add("data-submited");
+  /* hora de colocar as infos pra "dentro" */
+  div.innerHTML = `
+    <p>Nome: ${inputName}</p>
+    <p>Email: ${inputEmail}</p>
+    <p>CPF: ${inputCPF}</p>
+    <p>Endereço: ${inputEndereco}</p>
+    <p>Endereço: ${inputCidade}</p>
+    <p>Endereço: ${inputEstado}</p>
+    <p>Endereço: ${inputImovelCasa}</p>
+    <p>Endereço: ${inputImovelAPT}</p>
+  `;
+
+  dataArea.appendChild(div);
 };
 
+function submitData () {
+  const submitBtn = document.querySelector("#btn-form-submit");
+  submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    mountData();
+    clearData();
+  })
+};
