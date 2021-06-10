c5a0bb932b540d190dac4761419a34364bf2044b
Gustavo Pia <piagja@gmail.com>
2021-06-10 00:12:44 -0300

    Arrumou o HTML e o JS

--- aula6/aula6-1/exercise-6.1/scripts.js
+++ aula6/aula6-1/exercise-6.1/scripts.js
@@ -1,9 +1,17 @@
-const states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
-const select = document.querySelector("#input-estado");
+function populateSelect() {
+  const states = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO', 'DF'];
+  const select = document.querySelector("#input-estado");
+  
+  states.forEach((item) => {
+    const options = document.createElement("option");
+    options.innerHTML += `${item}`;
+    select.appendChild(options);
+  });
+};
 
-states.forEach((item) => {
-  const options = document.createElement("option");
-  options.innerHTML += `${item}`;
-  select.appendChild(options);
-});
+populateSelect();
+
+function submitData (event) {
+  event.preventDefault();
+};
 
