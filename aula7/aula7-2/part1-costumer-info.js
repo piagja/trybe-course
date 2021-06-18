8a3c281dac64a1dcaf9e978a4b70017a0a0a8c17
Gustavo Pia <piagja@gmail.com>
2021-06-18 19:49:22 -0300

    Realizou o segundo exercicio da parte 1

--- aula7/aula7-2/part1-costumer-info.js
+++ aula7/aula7-2/part1-costumer-info.js
@@ -38,7 +38,7 @@ const customerInfo = (order) => {
   // Adicione abaixo as informações necessárias.
   // "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
   const { name, phoneNumber, order: { delivery: { deliveryPerson }}, address: { street, number, apartment }} = order;
-  const message = `Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phoneNumber}, ${street}, Nº: ${number}, AP: ${apartment}`
+  const message = `Olá ${deliveryPerson}, entrega para: ${name}, Telefone: ${phoneNumber}, ${street}, Nº: ${number}, AP: ${apartment}`;
 
   return message;
 }
@@ -48,7 +48,14 @@ console.log(customerInfo(order))
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
+  // "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00."
+  order.name = 'Luiz Silva'
+  order.payment = 50;
+  const { name, payment, order: { pizza: { margherita, pepperoni }, drinks: { coke } } } = order;
+  const totalAntes = margherita.price + pepperoni.price + coke.price; // aqui só calcula o total dos valores antes da alteração
 
+  const message = `Olá ${name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${coke.type} é ${payment}.`
+  return message;
 }
 
-orderModifier(order);
+console.log(orderModifier(order))
