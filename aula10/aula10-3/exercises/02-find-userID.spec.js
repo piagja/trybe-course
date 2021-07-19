a33f588c440b1ead065bbe315e7373a913fe8bd4
Gustavo Pia <piagja@gmail.com>
2021-07-19 16:50:04 -0300

    Realizou exercicio 3 com async/await

--- aula10/aula10-3/exercises/02-find-userID.spec.js
+++ aula10/aula10-3/exercises/02-find-userID.spec.js
@@ -1,18 +1,40 @@
 const { getUserName } = require('./02-find-userID')
 
-describe('should retrieve user id', () => {
-  expect.assertions(2)
-  it('validate user id', () => {
-    expect(getUserName(4)).resolves.toEqual('Gustavo')
-    expect(getUserName(5)).resolves.toEqual('João')
+// promises + resolves + rejects
+// describe('should retrieve user id', () => {
+//   expect.assertions(2)
+//   it('validate user id', () => {
+//     expect(getUserName(4)).resolves.toEqual('Gustavo')
+//     expect(getUserName(5)).resolves.toEqual('João')
+//   })
+// })
+
+// describe('when passed a wrong id, should return an error', () => {
+//   it('should return an error message', () => {
+//     return getUserName(2).catch(error => {
+//       expect(error).toEqual(new Error('User with id 2 not found.'))
+//     }
+//     )
+//   })
+// })
+
+// async/await
+
+describe('async/await - should retrieve user id', () => {
+  it('validate user id', async () => {
+    expect.assertions(1)
+    const userName = await getUserName(4)
+    expect(userName).toEqual('Gustavo')
   })
 })
 
-describe('when passed a wrong id, should return an error', () => {
-  it('should return an error message', () => {
-    return getUserName(2).catch(error => {
+describe('async-await - should return an error when no id is passed', () => {
+  it('should return an error message', async () => {
+    expect.assertions(1)
+    try {
+      await getUserName(2)
+    } catch (error) {
       expect(error).toEqual(new Error('User with id 2 not found.'))
     }
-    )
   })
 })
