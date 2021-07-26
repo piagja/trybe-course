b28bad6a74dbb61416b96df70b89f447fac31317
Gustavo Pia <piagja@gmail.com>
2021-07-26 16:17:55 -0300

    Realizou mais exercicios de fixação

--- Frontend/aula11/aula11.3/states-events/src/App.js
+++ Frontend/aula11/aula11.3/states-events/src/App.js
@@ -1,51 +1,48 @@
 import React, { Component } from 'react'
-import colors from './data'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super()
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
 
     this.state = {
-      color: '',
+      count: 0
+    }
+  }
+
+  increment() {
+    console.log('Incremented by 1')
+    this.setState((prevState, _props) => ({
+      count: prevState.count + 1
+    }))
+  }
+
+  decrement() {
+    console.log('Decremented by 1')
+    this.setState((prevState, _props) => ({
+      count: prevState.count - 1
+    }))
+  }
+
+  buttonColor(num) {
+    if (num % 2 === 0) {
+      return 'green'
+    } else {
+      return 'red'
     }
   }
 
   render() {
-    const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
-
-    const setBackground = (color) => {
-      const body = document.getElementsByTagName('body')[0]
-      body.style.backgroundColor = color
-    }
-
     return (
-      <div className='App'>
-        <ul>
-          {colors
-          .filter((color) => color.name === this.state.color)
-          .map(({name, hex}, index) => (
-            <React.Fragment key={index}>
-              <li key={index}>Color Name: {name}</li>
-              <div style={{ backgroundColor: `${hex}`}} height='100px'>Corresponding Hex: {hex}</div>
-              <button style={{ margin: '15px 0' }}  onClick={() => setBackground(hex)}>Set Background</button>
-            </React.Fragment>
-          ))}
-        </ul>
-        <div>
-          <p>Filter colors by name (the colors above, in an array)</p>
-          <input type='text' onChange={(e) => this.setState({ color: capitalize(e.target.value) })} />
-        </div>
-
-        <hr />
-
-        <div>
-          <p>Type a color hex (Use #) or name, i will change the background to this color</p>
-          <input type='text' onChange={(e) => setBackground(e.target.value)} />
-        </div>
-
+      <div>
+        <button style={{backgroundColor: this.buttonColor(this.state.count)}} onClick={this.increment}>Increment</button>
+        <button style={{backgroundColor: this.buttonColor(this.state.count)}} onClick={this.decrement}>Decrement</button>
+        <p>Número de cliques: {this.state.count}</p>
       </div>
     )
   }
 }
 
-export default App
+export default App;
--- Frontend/aula11/aula11.3/states-events/src/App2.js
+++ Frontend/aula11/aula11.3/states-events/src/App2.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
+import colors from './data'
 
-export default function App1() {
-  return (
-    <div>
-      
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      color: '',
+    }
+  }
+
+  render() {
+    const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+    const setBackground = (color) => {
+      const body = document.getElementsByTagName('body')[0]
+      body.style.backgroundColor = color
+    }
+
+    return (
+      <div className='App'>
+        <ul>
+          {colors
+          .filter((color) => color.name === this.state.color)
+          .map(({name, hex}, index) => (
+            <React.Fragment key={index}>
+              <li key={index}>Color Name: {name}</li>
+              <div style={{ backgroundColor: `${hex}`}} height='100px'>Corresponding Hex: {hex}</div>
+              <button style={{ margin: '15px 0' }}  onClick={() => setBackground(hex)}>Set Background</button>
+            </React.Fragment>
+          ))}
+        </ul>
+        <div>
+          <p>Filter colors by name (the colors above, in an array)</p>
+          <input type='text' onChange={(e) => this.setState({ color: capitalize(e.target.value) })} />
+        </div>
+
+        <hr />
+
+        <div>
+          <p>Type a color hex (Use #) or name, i will change the background to this color</p>
+          <input type='text' onChange={(e) => setBackground(e.target.value)} />
+        </div>
+
+      </div>
+    )
+  }
 }
+
+export default App
