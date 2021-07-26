1713505ed2aba3fb9d6bae19253790187e2e1180
Gustavo Pia <piagja@gmail.com>
2021-07-26 15:25:54 -0300

    Fez alguns exercicios para fixaçao

---
+++ Frontend/aula11/aula11.3/states-events/src/data.js
@@ -0,0 +1,10 @@
+const colors = [
+  { name: 'Red', hex: '#f00' },
+  { name: 'Green', hex: '#0f0' },
+  { name: 'Blue', hex: '#00f' },
+  { name: 'Yellow', hex: '#ff0' },
+  { name: 'Cyan', hex: '#0ff' },
+  { name: 'Magenta', hex: '#f0f' },
+]
+
+export default colors
