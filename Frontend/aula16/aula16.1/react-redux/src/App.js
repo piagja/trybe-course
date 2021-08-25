eab1e45029306a80cba9a51c5e8e21c49286b9b4
Gustavo Pia <piagja@gmail.com>
2021-08-25 12:39:17 -0300

    Added changes to Redux environment

---
+++ Frontend/aula16/aula16.1/react-redux/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>meuapp</h1>
+      </div>
+    )
+  }
+}
