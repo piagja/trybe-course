75343e75a981c8e0d5dc012004241a1dacf39c22
piagja <piagja@gmail.com>
2022-04-04 19:46:26 -0300

    feat: done exercise 3 using types and interface

---
+++ Backend/aula26/exercicios-26.2/exercise3/index.ts
@@ -0,0 +1,20 @@
+import Pizza from './pizza'
+
+const Calabresa: Pizza = {
+  flavor: 'Calabresa',
+  slices: 8
+}
+
+const Marguerita: Pizza = {
+  flavor: 'Marguerita',
+  slices: 6
+}
+
+const Nutella: Pizza = {
+  flavor: 'Nutella',
+  slices: 4
+}
+
+console.log(Calabresa)
+console.log(Marguerita)
+console.log(Nutella)
