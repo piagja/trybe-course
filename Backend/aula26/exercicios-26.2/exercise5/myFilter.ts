5ab3529bb7cdb1c5a56e43c117c39b31fa647f8d
piagja <piagja@gmail.com>
2022-04-04 20:14:34 -0300

    feat: created myFilter funcion using TS features

--- Backend/aula26/exercicios-26.2/exercise5/myFilter.ts
+++ Backend/aula26/exercicios-26.2/exercise5/myFilter.ts
@@ -1,16 +1,33 @@
-const myFilter = (arr: any[], callback: Function): any => {
-  const newArr: any[] = []
+/**
+ * a HOC Filter leva tres parametros: valor, indice e array
+ * de cara da para criar um type para esses parametros
+ * usando Generics.
+ * Lembrando que o filter retorna um booleano (true, false)
+ * e que o segundo e terceiro parâmetros são opcionais
+ */
 
-  for (let i = 0; i > arr.length; i += 1) {
-    if (callback(arr[i])) {
+type filterCallback<T> = (value: T, index?: number, array?: T[]) => boolean
+
+function myFilter<T> (arr: Array<T>, callback: filterCallback<T>): Array<T> {
+  const newArr: Array<T> = []
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (callback(arr[i], i, arr)) {
       newArr.push(arr[i])
     }
-
-    return newArr
   }
+
+  return newArr
 }
 
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-const test = myFilter(arr, item => item % 2 === 0)
 
-console.log(test)
+// somente menores que 7
+console.log(
+  myFilter(arr, item => {
+    return item < 7
+  })
+)
+
+// somente pares
+console.log(myFilter(arr, item => item % 2 === 0))
