fbfbeff8392ad188868b52ad0d5b3b4bd0bff2d0
piagja <piagja@gmail.com>
2022-04-04 20:02:12 -0300

    feat: initiate myuFilter func with TS, type alias, unions and more

---
+++ Backend/aula26/exercicios-26.2/exercise5/myFilter.js
@@ -0,0 +1,13 @@
+"use strict";
+const myFilter = (arr, callback) => {
+    const newArr = [];
+    for (let i = 0; i > arr.length; i += 1) {
+        if (callback(arr[i])) {
+            newArr.push(arr[i]);
+        }
+        return newArr;
+    }
+};
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const test = myFilter(arr, item => item % 2 === 0);
+console.log(test);
